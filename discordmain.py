ab974d83f42fae56ff8459884822b14b2f6c49b1
luagugus <sosnm0907_@naver.com>
2021-06-06 11:16:23 +0900

    1.1v Embed로 깔끔하게

--- discordmain.py
+++ discordmain.py
@@ -9,12 +9,17 @@ class MyClient(discord.Client):
         print('실행중')
 
     async def on_message(self, message):
-        if message.content.startswith("!검색"): # 만약, 채팅이 "!도움말"로 >>시작<< 한다면,
+        if message.content.startswith("!검색"):
             value = message.content.replace('!검색', '')
             me = watcher.summoner.by_name(my_region, value)
             t = me['name']
             c = me['summonerLevel']
-            await message.channel.send("이름 : " + t + ", Lv." + c)
+            embed=discord.Embed(title="전적", description="전적입니다.", color=0x00aaaa)
+            embed.set_author(name="LOL BOT",icon_url=message.author.avatar_url)
+            embed.add_field(name="level", value=t, inline=False)
+            embed.add_field(name="name", value=c, inline=False)
+            embed.set_footer(text="togugu")
+            await message.channel.send(embed=embed)
                 
 client = MyClient()
 client.run('your token')
